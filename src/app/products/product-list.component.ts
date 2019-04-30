ede1128fab13d4976ed5f6a67c3a075a6a95b7d6
NilsCharlois <nils.charlois@gmail.com>
2019-04-30 13:31:43 +0100

    Section 9 done

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { IProduct } from "./products";
+import { ProductService } from "./product.service";
 
 @Component({
 selector:'pm-products',
@@ -17,37 +18,13 @@ export class ProductListComponent implements OnInit{
     get listFilter(): string { return this._listFilter}
     set listFilter(value: string) {
       this._listFilter = value;
-      this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products; 
+      this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this._productService.getProducts(); 
     }
 
     filteredProducts : IProduct[];
+    products : IProduct[];
 
-    products: IProduct[] = [
-        {
-          "productId": 1,
-          "productName": "Leaf Rake",
-          "productCode": "GDN-0011",
-          "releaseDate": "March 19, 2016",
-          "description": "Leaf rake with 48-inch wooden handle.",
-          "price": 19.95,
-          "starRating": 3.2,
-          "imageUrl": "https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-        },
-        {
-          "productId": 2,
-          "productName": "Garden Cart",
-          "productCode": "GDN-0023",
-          "releaseDate": "March 18, 2016",
-          "description": "15 gallon capacity rolling garden cart",
-          "price": 32.99,
-          "starRating": 4.2,
-          "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-        }
-    ];
-
-    constructor() {
-      this.filteredProducts = this.products;
-      this.listFilter = 'cart';
+    constructor(private _productService: ProductService) {
     }
 
     onRatingClicked(message: string) : void {
@@ -64,6 +41,7 @@ export class ProductListComponent implements OnInit{
     }
 
     ngOnInit(): void {
-      console.log('On Init');
+      this.products = this._productService.getProducts();
+      this.filteredProducts = this.products;
     }
 }
