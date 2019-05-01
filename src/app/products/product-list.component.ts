d28f55c185a1aa498008e768572cc05a82d48ddd
NilsCharlois <nils.charlois@gmail.com>
2019-05-01 21:03:54 +0100

    bug fix

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'
+
 import { IProduct } from "./products";
 import { ProductService } from "./product.service";
 
@@ -13,12 +14,13 @@ export class ProductListComponent implements OnInit{
     imageWidth: number = 50;
     imageMargin : number = 2;
     showImage: boolean = false;
+    errorMessage: string;
 
     _listFilter: string;
     get listFilter(): string { return this._listFilter}
     set listFilter(value: string) {
       this._listFilter = value;
-      this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this._productService.getProducts(); 
+      this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products; 
     }
 
     filteredProducts : IProduct[];
@@ -41,7 +43,12 @@ export class ProductListComponent implements OnInit{
     }
 
     ngOnInit(): void {
-      this.products = this._productService.getProducts();
-      this.filteredProducts = this.products;
+      this._productService.getProducts().subscribe(
+        products => {
+          this.products = products;
+          this.filteredProducts = this.products;
+        },
+        error => this.errorMessage = <any>error
+      );
     }
 }
